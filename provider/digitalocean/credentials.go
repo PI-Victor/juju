1591f7c01b1330659cfdcd49a095788578d7a8ee
Victor Palade <victor@cloudflavor.io>
2018-09-05 09:27:38 +0200

    *: try to satisfy basic interfaces.

--- provider/digitalocean/credentials.go
+++ provider/digitalocean/credentials.go
@@ -1,3 +1,20 @@
 package digitalocean
 
+import (
+	"github.com/juju/juju/cloud"
+	"github.com/juju/juju/environs"
+)
+
 type environProviderCredentials struct{}
+
+func (environProviderCredentials) CredentialSchemas() map[cloud.AuthType]cloud.CredentialSchema {
+	return map[cloud.AuthType]cloud.CredentialSchema{}
+}
+
+func (environProviderCredentials) DetectCredentials() (*cloud.CloudCredential, error) {
+	return nil, nil
+}
+
+func (environProviderCredentials) FinalizeCredential(_ environs.FinalizeCredentialContext, _ environs.FinalizeCredentialParams) (*cloud.Credential, error) {
+	return nil, nil
+}
--- provider/digitalocean/provider.go
+++ provider/digitalocean/provider.go
@@ -1,6 +1,11 @@
 package digitalocean
 
 import (
+	"context"
+
+	"golang.org/x/oauth2"
+
+	"github.com/juju/jsonschema"
 	"github.com/juju/loggo"
 
 	"github.com/digitalocean/godo"
@@ -8,6 +13,18 @@ import (
 )
 
 var logger = loggo.GetLogger("juju.provider.digitalocean")
+
+type tokenSource struct {
+	AccessToken string
+}
+
+// NOTE: done to satisfy the token interface for oauth2.
+func (t *tokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
 
 type environProvider struct {
 	environProviderCredentials
@@ -25,10 +42,24 @@ func (p environProvider) Open(args environs.OpenParams) (environs.Environ, error
 	e := new(environ)
 	e.cloud = args.Cloud
 	e.name = args.Config.Name()
-
+	e.dgo = newDGOClient(e.cloud)
+	return e, nil
 }
 
-func digitalOceanClient(cloud environs.CloudSpec) (*godo.Client, error) {
+func newDGOClient(cloud environs.CloudSpec) *godo.Client {
+	creds := cloud.Credential.Attributes()
+	t := &tokenSource{
+		// TODO: pull this from the config
+		AccessToken: creds["AccessToken"],
+	}
+	newOauth2 := oauth2.NewClient(context.Background(), t)
+	return godo.NewClient(newOauth2)
+}
 
+func (p environProvider) CloudSchema() *jsonschema.Schema {
+	return nil
+}
+
+func newDOClient(cloud environs.CloudSpec) (*godo.Client, error) {
 	return nil, nil
 }
